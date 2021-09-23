36624564daf2d2d052857bbbcd5eea3c4c6216b9
RadoMiami <kguthre@gmail.com>
2021-09-23 13:11:57 -0400

    Changed back exercise25, misunderstood how to use enums

--- exercise25/src/main/java/baseline/PasswordStrengthValueMeaning.java
+++ exercise25/src/main/java/baseline/PasswordStrengthValueMeaning.java
@@ -4,12 +4,14 @@
  */
 package baseline;
 
-enum PasswordStrengthValueMeaning {;
-
+final class PasswordStrengthValueMeaning {
+    
     public static final int VERY_WEAK_PASS_NUM = 1;
     public static final int WEAK_PASS_NUM = 2;
     public static final int STRONG_PASS_NUM = 3;
     public static final int VERY_STRONG_PASS_NUM = 4;
 
     //0 = unknown, you should be able to figure out the rest
+
+    private PasswordStrengthValueMeaning() {}
 }
