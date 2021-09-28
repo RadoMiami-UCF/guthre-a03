97ac91157851c6700a5d670f891cad6ccc5a58a8
RadoMiami <kguthre@gmail.com>
2021-09-28 14:47:36 -0400

    Skeleton code and pseudocode for exercise29

---
+++ exercise29/src/main/java/baseline/ReturnValidator.java
@@ -0,0 +1,12 @@
+package baseline;
+
+final class ReturnValidator {
+
+    private ReturnValidator() {}
+
+    public static double validateInput() {
+        //If the rate of return is an invalid input, then say that it isn't a valid input and ask again.
+        //If the rate of return is a valid input, then return it in double form.
+        return 0;
+    }
+}
---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Kimari Guthre
+ */
+package baseline;
+
+public class Solution29 {
+    public static void main(String[] args) {
+        //First, ask for a rate of return through ReturnValidator.validateInput().
+        //Then, divide 72 by that rate of return to get the approximate amount of years to double your investment.
+        //Finally, return that amount of years (rounded up).
+    }
+}
