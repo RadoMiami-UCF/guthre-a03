a50b69e2c28e809ab8efdc58f77ecd749942bd2e
RadoMiami <kguthre@gmail.com>
2021-09-29 19:19:43 -0400

    Fixed copyright info in exercise32 and made pseudocode + skeleton code.

---
+++ exercise33/src/main/java/baseline/Magic8Ball.java
@@ -0,0 +1,9 @@
+package baseline;
+
+final class Magic8Ball {
+    //First, use a map to store the numbers 0-3 and assign them to the four different entries in the Magic 8 Ball.
+    public static String get8BallString(int randNum) {
+        //Reply with the string that corresponds with that number through the map.
+        return "";
+    }
+}
---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Kimari Guthre
+ */
+package baseline;
+
+public class Solution33 {
+    public static void main(String[] args) {
+        //Then, ask for a question. (Probably don't have to do anything with it.)
+        //Then, use an object of type Random to get a number from 0-3. We'll call this number randNum.
+        //Finally, print the string that comes from Magic8Ball.get8BallString(randNum).
+    }
+}
