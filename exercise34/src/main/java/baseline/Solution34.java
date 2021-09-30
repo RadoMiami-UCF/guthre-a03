3607790dcc766d69c72aed89a130c6571ef21392
RadoMiami <kguthre@gmail.com>
2021-09-29 20:15:34 -0400

    Fixed copyright info for exercise33 and made skeleton code and pseudocode for exercise34.

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Kimari Guthre
+ */
+package baseline;
+
+import java.util.ArrayList;
+
+public class Solution34 {
+    public static void main(String[] args) {
+        //There actually isn't anything I can test here, since I'm just using functions that already exist and work.
+        //Furthermore, given the simplicity of 34, I can go back to one-class solutions!
+        //First, make an arraylist containing the 5 names.
+        //Then, call sayArrayList(employeeArrayList).
+        //Then, ask for an employee name to remove.
+        //Then, remove the employee using remove(employeeName), and use trimToSize if necessary.
+        //Finally, call sayArrayList(employeeArrayList) again.
+    }
+
+    private void sayArrayList(ArrayList arrayListToSay) {
+        //First, say the number of employees in the arraylist.
+        //Then, use a loop to iterate through all the objects in the arraylist and say each one.
+    }
+}
