a7b46d0232f42252f4a903f07b99d20e0fcbf54f
RadoMiami <kguthre@gmail.com>
2021-09-30 13:06:11 -0400

    Finished exercise35

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -4,13 +4,51 @@
  */
 package baseline;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.*;
+
 public class Solution35 {
+    public static final Scanner in = new Scanner(System.in, StandardCharsets.UTF_8);
+
     public static void main(String[] args) {
+        //Can't write any test cases here for the same reason as last time: too simple/something that can't easily
+        //be tested.
         //First, create an empty arraylist.
-        //Then, begin a loop that only ends when the in.nextLine() returns empty. (Do while?)
-        //In that loop, ask for a name, then store it.
+        ArrayList<String> entrantList = new ArrayList<>();
+
+        //Then, begin a loop that only ends when the in.nextLine() returns empty.
+        while(true) {
+            //In that loop, ask for a name, then store it in the array list.
+            System.out.print("Enter a name: ");
+            var nextName = in.nextLine();
+            if("".equals(nextName)) {
+                break;
+            } else {
+                entrantList.add(nextName);
+            }
+        }
+
         //After the loop, trim the arraylist's size.
-        //Then, use an object of type Random to get a random number between 0 and the size of the array - 1.
-        //Then, say that the string at that index in the arraylist is the winner.
+        entrantList.trimToSize();
+
+        //Using this simple program as an excuse to learn how to exception handle properly.
+        //Note: using loggers wasn't as bad as I thought! Why wasn't I always doing this?
+        try {
+            //Then, use an object of type Random to get a random number between 0 and the size of the array - 1 and say
+            //that the string at that index in the arraylist is the winner.
+            var randomNumberGenerator = new Random();
+            //Then, say that the string at that index in the arraylist is the winner.
+            System.out.print("The winner is... " + entrantList.get(randomNumberGenerator.nextInt(entrantList.size()))
+                    + ".");
+        } catch (IllegalArgumentException exception) {
+            var logger = Logger.getLogger("logger");
+            logger.log(WARNING, "No names were entered", exception);
+            System.out.print("You must enter at least one name!");
+        }
     }
 }
