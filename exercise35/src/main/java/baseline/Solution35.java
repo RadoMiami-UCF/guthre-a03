3de35c146fd790c12845f0f0339a712c41968636
RadoMiami <kguthre@gmail.com>
2021-09-30 11:55:37 -0400

    Pseudocode for exercise35

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Kimari Guthre
+ */
+package baseline;
+
+public class Solution35 {
+    public static void main(String[] args) {
+        //First, create an empty arraylist.
+        //Then, begin a loop that only ends when the in.nextLine() returns empty. (Do while?)
+        //In that loop, ask for a name, then store it.
+        //After the loop, trim the arraylist's size.
+        //Then, use an object of type Random to get a random number between 0 and the size of the array - 1.
+        //Then, say that the string at that index in the arraylist is the winner.
+    }
+}
