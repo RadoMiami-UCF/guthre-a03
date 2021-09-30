2d139b2bc1602ffa84d203530915d791c730fa76
RadoMiami <kguthre@gmail.com>
2021-09-30 18:50:15 -0400

    New .gitignore should be working now

--- exercise31/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,9 +0,0 @@
-
-[
-+src/main/java/baseline/TargetHeartRate.java,b\9\b902f1327a5c0b1e79705cd033500f3efd7f4959
-_
-/src/test/java/baseline/TargetHeartRateTest.java,9\9\99a28550864f3ec148b86ab2ea53bfddae5aeae6
-<
-build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
-V
-&src/main/java/baseline/Solution31.java,5\c\5cafb99e82926136c2f1d044c79185ad9c24e74e
--- exercise36/src/main/java/baseline/Solution36.java
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -4,13 +4,50 @@
  */
 package baseline;
 
+import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Solution36 {
+    private static final Scanner in = new Scanner(System.in, StandardCharsets.UTF_8);
+    private static final DecimalFormat doubleFormat = new DecimalFormat("0.##");
+    
     public static void main(String[] args) {
         //First, create a new arrayList.
+        ArrayList<Double> doubleList = new ArrayList<>();
         //Then, use a while loop to populate said list with doubles inputted from the user.
+        while(true) {
+            System.out.print("Enter a number: ");
+            var inString = in.nextLine();
+            if(Pattern.matches("\\d+", inString)) {
+                doubleList.add(Double.parseDouble(inString));
+            } else if("done".equals(inString)) {
+                break;
+            }
+        }
         //When the user inputs "done" instead of another double, break the loop.
         //After the loop, trim the arraylist's size.
-        //Then, repeat the numbers to the user.
+        doubleList.trimToSize();
+        //Then, print doubleList through printNumbers.
+        printNumbers(doubleList);
         //Finally, say the results of StatFunctions.average, min, max, and std.
+        System.out.print("The average is ")
+    }
+
+    private static void printNumbers(List<Double> doubleList) {
+        //First, say "Numbers: ".
+        System.out.print("Numbers:");
+        //Then, print each of the numbers in doubleList.
+        for(var j = 0; j < doubleList.size(); j++) {
+            //First, print a space, then the number in the index j of doubleList.
+            System.out.print(" " + doubleList.get(j));
+            //If it isn't the last one, also print ",".
+            if(j < doubleList.size() - 1) {
+                System.out.print(",");
+            }
+        }
     }
 }
