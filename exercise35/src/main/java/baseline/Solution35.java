c75689656cd2108e87495e6f3e422be49e33e1e9
RadoMiami <kguthre@gmail.com>
2021-09-30 13:27:18 -0400

    Destinked exercise35

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -6,8 +6,10 @@ package baseline;
 
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.List;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.*;
@@ -21,29 +23,47 @@ public class Solution35 {
         //First, create an empty arraylist.
         ArrayList<String> entrantList = new ArrayList<>();
 
-        //Then, begin a loop that only ends when the in.nextLine() returns empty.
+        /*
+        Then, begin a loop that only ends when the in.nextLine() returns empty.
+        I could have compressed this into a single while line, but apparently I have to follow a "style guide" or
+        something lame like that.
+         */
         while(true) {
-            //In that loop, ask for a name, then store it in the array list.
-            System.out.print("Enter a name: ");
-            var nextName = in.nextLine();
-            if("".equals(nextName)) {
+            if (!hasNewName(entrantList)) {
                 break;
-            } else {
-                entrantList.add(nextName);
             }
         }
 
         //After the loop, trim the arraylist's size.
         entrantList.trimToSize();
 
+        //Finally, call pickRandomName to pick and say a random name.
+        var randomNumberGenerator = new Random();
+        pickRandomName(entrantList, randomNumberGenerator);
+    }
+
+    public static boolean hasNewName(Collection<String> stringArrayList) {
+        System.out.print("Enter a name: ");
+        var nextName = in.nextLine();
+        if("".equals(nextName)) {
+            return false;
+        } else {
+            stringArrayList.add(nextName);
+            return true;
+        }
+    }
+
+    public static void pickRandomName(List<String> nameList, Random randomNumberGenerator) {
         //Using this simple program as an excuse to learn how to exception handle properly.
         //Note: using loggers wasn't as bad as I thought! Why wasn't I always doing this?
+
         try {
-            //Then, use an object of type Random to get a random number between 0 and the size of the array - 1 and say
-            //that the string at that index in the arraylist is the winner.
-            var randomNumberGenerator = new Random();
+            /*
+            Use an object of type Random to get a random number between 0 and the size of the array - 1 and say
+            that the string at that index in the arraylist is the winner.
+             */
             //Then, say that the string at that index in the arraylist is the winner.
-            System.out.print("The winner is... " + entrantList.get(randomNumberGenerator.nextInt(entrantList.size()))
+            System.out.print("The winner is... " + nameList.get(randomNumberGenerator.nextInt(nameList.size()))
                     + ".");
         } catch (IllegalArgumentException exception) {
             var logger = Logger.getLogger("logger");
