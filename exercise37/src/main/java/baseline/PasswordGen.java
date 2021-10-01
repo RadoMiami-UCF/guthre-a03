6ad53fc310e3557449dc012a419861aba8cc46b3
RadoMiami <kguthre@gmail.com>
2021-10-01 16:38:25 -0400

    Refactored pseudocode because it has to use arraylists and I didn't catch that when I read the exercise

--- exercise37/src/main/java/baseline/PasswordGen.java
+++ exercise37/src/main/java/baseline/PasswordGen.java
@@ -1,21 +1,22 @@
 package baseline;
 
+import java.util.ArrayList;
+
 final class PasswordGen {
     private PasswordGen() {}
 
     public static String generatePassword(int minLength, int specialCharacters, int numbers) {
-        //First, generate the amount of special characters and add that to a string.
-        //Then, generate the amount of numbers and add that to the string.
-        //Then, generate enough letters to get to the minimum length and add them to the string.
+        //First, generate the amount of special characters and add that to a list of characters.
+        //Then, generate the amount of numbers and add that to the list.
+        //Then, generate enough letters to get to the minimum length and add them to the list.
         //Then, use PasswordGen.shufflePass() to shuffle the password.
-        //Finally, return the shuffledString.
+        //Finally, return the scrambled password.
         return "";
     }
 
-    private static String shufflePassword(String stringToShuffle) {
-        //First, convert the string into a StringBuilder.
-        //Then, toss it into a while loop:
-        //While the StringBuilder isn't empty, add a random character to a string, then remove that character.
+    private static String shufflePassword(ArrayList<Character> arrayOfCharacters) {
+        //Toss the array of characters into a while loop:
+        //While the arraylist isn't empty, add a random character to a string, then remove that character.
         //After the while loop is over, return the scrambled string.
         return "";
     }
