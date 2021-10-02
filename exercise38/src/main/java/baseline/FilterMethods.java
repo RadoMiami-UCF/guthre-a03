fb2c7783156981fb07c4b4b4f19c36df1252fb1a
RadoMiami <kguthre@gmail.com>
2021-10-02 18:15:51 -0400

    Pseudocode for exercise38

---
+++ exercise38/src/main/java/baseline/FilterMethods.java
@@ -0,0 +1,13 @@
+package baseline;
+
+class FilterMethods {
+    private FilterMethods() {}
+
+    public static int[] filterEvenNumbers(String[] numbersToFilter) {
+        //First, create an array of integers. We'll call it evenNumberList.
+        //Then, for each string given, convert it into an integer.
+        //If the integer is even, add it to evenNumberList.
+        //After the loop, return evenNumberList.
+        return new int[]{0};
+    }
+}
---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,10 @@
+package baseline;
+
+public class Solution38 {
+    public static void main(String[] args) {
+        //First, ask for a list of numbers.
+        //Then, split that list into an array of strings using String.split(" ", 0).
+        //Then, pass that array into FilterMethods.filterEvenNumbers.
+        //Finally, print out the resulting array of integers.
+    }
+}
