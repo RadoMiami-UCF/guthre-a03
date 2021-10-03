6bdf6f6e8acd2b322f2733fe625efc288ce43957
RadoMiami <kguthre@gmail.com>
2021-10-03 18:53:44 -0400

    Finished exercise39

--- exercise39/src/main/java/baseline/Solution39.java
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -1,11 +1,91 @@
 package baseline;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Map;
 
 public class Solution39 {
+    //WOW, this was a doozy. Very glad that it's over now.
+    /*
+    I'm honestly not satisfied with this implementation. There's no reason that this couldn't have been a single map,
+    or simply an arrayList. This is just wasteful! However, they asked for a list of maps, so I'm giving them a list
+    of maps.
+     */
+    private static final int SPACES_TO_ADD_TO_NAME = 2;
+    private static final int MINIMUM_NAME_LENGTH = 5;
+    private static final int MINIMUM_POSITION_LENGTH = 9;
+    private static final Map<Integer, EmployeeData> JOHN_JOHNSON_MAP = Map.of(0, new EmployeeData(
+            "John", "Johnson", "Manager", 31, Calendar.DECEMBER,
+            2016));
+    private static final Map<Integer, EmployeeData> TOU_XIONG_MAP = Map.of(0, new EmployeeData(
+            "Tou", "Xiong", "Software Engineer", 5, Calendar.OCTOBER,
+            2016));
+    private static final Map<Integer, EmployeeData> MICHAELA_MICHAELSON_MAP = Map.of(0, new EmployeeData(
+            "Michaela", "Michealson", "District Manager", 19, Calendar.DECEMBER,
+            2015));
+    private static final Map<Integer, EmployeeData> JAKE_JACOBSON_MAP = Map.of(0, new EmployeeData(
+            "Jake", "Jacobson", "Programmer"));
+    private static final Map<Integer, EmployeeData> JACQUELYN_JACKSON_MAP = Map.of(0, new EmployeeData(
+            "Jacquelyn", "Jackson", "DBA"));
+    private static final Map<Integer, EmployeeData> SALLY_WEBBER_MAP = Map.of(0, new EmployeeData(
+            "Sally", "Webber", "Web Developer", 18, Calendar.DECEMBER,
+            2015));
+
     public static void main(String[] args) {
-        //First, create a map that has an integer as the key, and an instance of EmployeeData as the value.
-        //Then, sort the list based on each map's EmployeeData.lastName.
-        //Finally, tell each map to the user, in the order that the list is in.
+        //First, create a list that the maps will be stored in.
+        //Then, create a series of maps that has the last name as the key, and an instance of EmployeeData as the value.
+        //The above was done in the form of final class variables.
+        //Then, store the maps in the list.
+        ArrayList<Map<Integer, EmployeeData>> mapList = new ArrayList<>(Arrays.asList(JOHN_JOHNSON_MAP, TOU_XIONG_MAP,
+                MICHAELA_MICHAELSON_MAP, JAKE_JACOBSON_MAP, JACQUELYN_JACKSON_MAP, SALLY_WEBBER_MAP));
+        //Then, sort the list based on each map's data. See EmployeeData.compareTo for more details.
+        mapList.sort(new EmployeeDataComparator());
+        //Then, trim the arrayList to make sure that it's only large enough to store its list of maps.
+        mapList.trimToSize();
+        //Then, find the length of the longest name in the list and the length of the longest position for formatting.
+        var lengthOfLongestName = 0;
+        var lengthOfLongestPosition = 0;
+
+        for(Map<Integer, EmployeeData> integerEmployeeDataMap : mapList) {
+            if(integerEmployeeDataMap.get(0).getFirstName().length() +
+                    integerEmployeeDataMap.get(0).getLastName().length() +
+                    SPACES_TO_ADD_TO_NAME > lengthOfLongestName) {
+                lengthOfLongestName = integerEmployeeDataMap.get(0).getFirstName().length() +
+                        integerEmployeeDataMap.get(0).getLastName().length() + SPACES_TO_ADD_TO_NAME;
+            }
+
+            if(integerEmployeeDataMap.get(0).getPosition().length() + 1 > lengthOfLongestPosition) {
+                lengthOfLongestPosition = integerEmployeeDataMap.get(0).getPosition().length() + 1;
+            }
+        }
+
+        if(lengthOfLongestName < MINIMUM_NAME_LENGTH) {
+            lengthOfLongestName = MINIMUM_NAME_LENGTH;
+        }
+
+        if(lengthOfLongestPosition < MINIMUM_POSITION_LENGTH) {
+            lengthOfLongestPosition = MINIMUM_POSITION_LENGTH;
+        }
+        //Finally, build and return the chart.
+        System.out.print(buildChart(mapList, lengthOfLongestName, lengthOfLongestPosition));
+    }
+
+    private static String buildChart(ArrayList<Map<Integer, EmployeeData>> mapArrayList, int lengthOfLongestName,
+                                     int lengthOfLongestPosition) {
+        //First, create a string builder.
+        var chartBuilder = new StringBuilder();
+        //Then, create the header, adding the correct amount of spaces for the name and position columns.
+        chartBuilder.append("Name").append(" ".repeat(lengthOfLongestName - (MINIMUM_NAME_LENGTH - 1)))
+                .append("| Position").append(" ".repeat(lengthOfLongestPosition - (MINIMUM_POSITION_LENGTH - 1)))
+                .append("| Separation Date\n");
+        //Then, create the correct amount of hyphens, punctuated by vertical lines.
+        chartBuilder.append("-".repeat(lengthOfLongestName)).append("|").append("-".repeat(lengthOfLongestPosition + 1))
+                .append("|").append("----------------");
+        for(Map<Integer, EmployeeData> integerEmployeeDataMap : mapArrayList) {
+            chartBuilder.append("\n").append(integerEmployeeDataMap.get(0)
+                    .toStringForChart(lengthOfLongestName, lengthOfLongestPosition));
+        }
+        return chartBuilder.toString();
     }
 }
---
+++ exercise39/src/test/java/baseline/EmployeeDataTest.java
@@ -0,0 +1,42 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeDataTest {
+
+    @Test
+    void testToStringForChart() {
+        var testEmployeeData = new EmployeeData("Bob", "Chris", "Intern");
+        assertEquals("Bob Chris | Intern |",
+                testEmployeeData.toStringForChart(10, 7),
+                "Employee data not printed properly #1");
+        assertEquals("Bob Chris    | Intern   |",
+                testEmployeeData.toStringForChart(13, 9),
+                "Employee data not printed properly #2");
+        testEmployeeData = new EmployeeData("Steve", "Smith", "Head Teller", 12,
+                Calendar.DECEMBER, 2021);
+        assertEquals("Steve Smith | Head Teller | 2021-12-12",
+                testEmployeeData.toStringForChart(12, 12),
+                "Employee separation date not printed properly");
+    }
+
+    @Test
+    void testCompareTo() {
+        var testEmployeeDataSmall = new EmployeeData("Aaron", "Andrew", "Lemon Supplier");
+        var testEmployeeDataLarge = new EmployeeData("Z I don't know any Z first names", "Zebra",
+                "Intern again");
+        var testEmployeeDataSameAsSmall = new EmployeeData("Aaron", "Andrew",
+                "Lemon Supplier");
+        assertTrue(testEmployeeDataSmall.compareTo(testEmployeeDataLarge) < 0,
+                "Employee did not properly identify as smaller");
+        assertTrue(testEmployeeDataLarge.compareTo(testEmployeeDataSmall) > 0,
+                "Employee did not properly identify as larger");
+        assertEquals(0, testEmployeeDataSmall.compareTo(testEmployeeDataSameAsSmall),
+                "Employee did not properly identify as the same");
+    }
+    //Equals isn't tested because it isn't even used.
+}
